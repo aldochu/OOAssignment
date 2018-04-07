dcd6f0976026c20dbd0621dee8620b9b7c381aaf
aldochu <aldochu@gmail.com>
2018-04-07 15:51:29 +0800

    Add parent class registration to reservation class

---
+++ src/HRPS/Registration.java
@@ -0,0 +1,13 @@
+package HRPS;
+
+import java.util.Date;
+
+public class Registration {
+	public String res_id;
+	public String guestId;
+	public Date check_in;
+	public Date check_out;
+	public int NoOfAdult;
+	public int NoOfChild;
+	public String roomType,bedType;
+}
--- src/HRPS/Reservation.java
+++ src/HRPS/Reservation.java
@@ -2,22 +2,9 @@ package HRPS;
 
 import java.util.Date;
 
-import HRPS.CreditCard;
-import HRPS.Guest;
-import HRPS.GuestData;
-
-public class Reservation {
+public class Reservation extends Registration {
 	
-	public String res_id,guestId;
-	public Date check_in;
-	public Date check_out;
-	public int NoOfAdult;
-	public int NoOfChild;
-	public String roomType,bedType;
 	public int status;
 	Boolean roomStatus;
-	
-	
-
 }
 
