60d4752900fc884f38be08555956319195b9c8ff
aldochu <aldochu@gmail.com>
2018-04-11 01:25:41 +0800

    Simplified method for checking reservation

--- src/HRPS/Validation.java
+++ src/HRPS/Validation.java
@@ -58,7 +58,7 @@ public class Validation {
 
 	}
 	
-	public String CheckDateWithinWithRoomNo(Date D,ArrayList<Reservation> R, String type) //this will check D with the current date + k
+	public String CheckRoomVacancyFromReservation(Date D,ArrayList<Reservation> R, String type) //this will check D with the current date + k
 	{	
 		
 		
@@ -67,307 +67,18 @@ public class Validation {
 		{
 		case "1": //VIP room 07-01 - 07-02
 			
-			//1st check whether there's any reservation for this room
-			int floor = 7;
-			int room = 1;
-			
-			int totalNoOfRoom = 2;
-			boolean roomBook=false; //this is to check whether there's this room in the reservation
-			
-			for(int i=0;i<totalNoOfRoom;i++) //this forloop will run through all room
-			{
-				String roomNo = "0"+floor + "-" + "0"+room;
-				for(int k=0;k<R.size();k++)
-				{
-					
-					//1st check whether there's any reservation for this room
-					Reservation temp = R.get(k);
-					
-					if(temp.bedType!=null)
-					{
-						if(temp.bedType.equals(roomNo))
-						{
-							roomBook=true; //this room exist
-						}
-					}
-					
-				}
-				
-				if(roomBook==true)
-				{
-				room++;
-				roomBook=false; //reset counter
-				}
-				else
-					return roomNo; //there's no this room in reservation so can book
-			}
-			
-			
-			
-			//2nd if the room exist in reservation, then check their date
-			floor = 7;
-			room = 1;
-			
-			totalNoOfRoom = 2;
-			
-			for(int i=0;i<totalNoOfRoom;i++) //this forloop will run through all room
-			{
-				String roomNo = "0"+floor + "-" + "0"+room; //format roomno as string
-				for(int k=0;k<R.size();k++)
-				{
-					//1st check whether there's any reservation for this room
-					Reservation temp = R.get(k);
-					
-					if(temp.bedType!=null) //to ensure that the roomtype is not null
-					{
-						
-						if(temp.bedType.equals(roomNo))//if it's the same room
-						{
-							if(!(D.compareTo(temp.check_in)>=0 && D.compareTo(temp.check_out)<0))
-							{
-								return roomNo;
-							
-							}
-						}
-			
-					}		
-				}
-				room++;
-			}			
-			break;	
-			
-			
+			return SearchRoomFunction(D,R,7,1,1,2);
 		case "2":  //Deluxe| room 07-03 - 07-08
 			
-			//1st check whether there's any reservation for this room
-			floor = 7;
-			room = 3;
-			
-			totalNoOfRoom = 6;
-			roomBook=false; //this is to check whether there's this room in the reservation
-			
-			for(int i=0;i<totalNoOfRoom;i++) //this forloop will run through all room
-			{
-				String roomNo = "0"+floor + "-" + "0"+room;
-				for(int k=0;k<R.size();k++)
-				{
-					
-					//1st check whether there's any reservation for this room
-					Reservation temp = R.get(k);
-					
-					if(temp.bedType!=null)
-					{
-						if(temp.bedType.equals(roomNo))
-						{
-							roomBook=true; //this room exist
-						}
-					}
-					
-				}
-				
-				if(roomBook==true)
-				{
-				room++;
-				roomBook=false; //reset counter
-				}
-				else
-					return roomNo; //there's no this room in reservation so can book
-			}
-			
-			
-			
-			//2nd if the room exist in reservation, then check their date
-			floor = 7;
-			room = 3;
-			
-			totalNoOfRoom = 6;
-			
-			for(int i=0;i<totalNoOfRoom;i++) //this forloop will run through all room
-			{
-				String roomNo = "0"+floor + "-" + "0"+room; //format roomno as string
-				for(int k=0;k<R.size();k++)
-				{
-					//1st check whether there's any reservation for this room
-					Reservation temp = R.get(k);
-					
-					if(temp.bedType!=null) //to ensure that the roomtype is not null
-					{
-						
-						if(temp.bedType.equals(roomNo))//if it's the same room
-						{
-							if(!(D.compareTo(temp.check_in)>=0 && D.compareTo(temp.check_out)<0))
-							{
-								return roomNo;
-							
-							}
-						}
-			
-					}		
-				}
-				room++;
-			}			
-			break;
+			return SearchRoomFunction(D,R,7,3,1,6);
 			
 		case "3":  //Single room 02-01 - 02-08 & 03-01 - 03-08 
 			
-			//1st check whether there's any reservation for this room
-			floor = 2;
-			room = 1;
-			
-			int totalNoOffloor = 2;
-			totalNoOfRoom = 8;
-			roomBook=false; //this is to check whether there's this room in the reservation
-			
-			for(int t=0;t<totalNoOffloor;t++)
-			{
-				for(int i=0;i<totalNoOfRoom;i++) //this forloop will run through all room
-				{
-					String roomNo = "0"+floor + "-" + "0"+room;
-					for(int k=0;k<R.size();k++)
-					{
-						
-						//1st check whether there's any reservation for this room
-						Reservation temp = R.get(k);
-						
-						if(temp.bedType!=null)
-						{
-							if(temp.bedType.equals(roomNo))
-							{
-								roomBook=true; //this room exist
-							}
-						}
-						
-					}
-					
-					if(roomBook==true)
-					{
-					room++;
-					roomBook=false; //reset counter
-					}
-					else
-						return roomNo; //there's no this room in reservation so can book
-				}
-				floor++;
-			}
-			
-			
-			
-			//2nd if the room exist in reservation, then check their date
-			floor = 2;
-			room = 1;
-			
-			totalNoOffloor = 2;
-			totalNoOfRoom = 8;
-			for(int t=0;t<totalNoOffloor;t++)
-			{
-				for(int i=0;i<totalNoOfRoom;i++) //this forloop will run through all room
-				{
-					String roomNo = "0"+floor + "-" + "0"+room; //format roomno as string
-					for(int k=0;k<R.size();k++)
-					{
-						//1st check whether there's any reservation for this room
-						Reservation temp = R.get(k);
-						
-						if(temp.bedType!=null) //to ensure that the roomtype is not null
-						{
-							
-							if(temp.bedType.equals(roomNo))//if it's the same room
-							{
-								if(!(D.compareTo(temp.check_in)>=0 && D.compareTo(temp.check_out)<0))
-								{
-									return roomNo;
-								
-								}
-							}
-				
-						}		
-					}
-					room++;
-				}	
-				floor++;
-			}
-			break;
+			return SearchRoomFunction(D,R,2,1,2,8);
 			
 		case "4":  //Single room 04-01 - 04-08 & 05-01 - 05-08 & 06-01 - 06-08 
 			
-			//1st check whether there's any reservation for this room
-			floor = 4;
-			room = 1;
-			
-			totalNoOffloor = 3;
-			totalNoOfRoom = 8;
-			roomBook=false; //this is to check whether there's this room in the reservation
-			
-			for(int t=0;t<totalNoOffloor;t++)
-			{
-				for(int i=0;i<totalNoOfRoom;i++) //this forloop will run through all room
-				{
-					String roomNo = "0"+floor + "-" + "0"+room;
-					for(int k=0;k<R.size();k++)
-					{
-						
-						//1st check whether there's any reservation for this room
-						Reservation temp = R.get(k);
-						
-						if(temp.bedType!=null)
-						{
-							if(temp.bedType.equals(roomNo))
-							{
-								roomBook=true; //this room exist
-							}
-						}
-						
-					}
-					
-					if(roomBook==true)
-					{
-					room++;
-					roomBook=false; //reset counter
-					}
-					else
-						return roomNo; //there's no this room in reservation so can book
-				}
-				floor++;
-			}
-			
-			
-			
-			//2nd if the room exist in reservation, then check their date
-			floor = 4;
-			room = 1;
-			
-			totalNoOffloor = 2;
-			totalNoOfRoom = 8;
-			for(int t=0;t<totalNoOffloor;t++)
-			{
-				for(int i=0;i<totalNoOfRoom;i++) //this forloop will run through all room
-				{
-					String roomNo = "0"+floor + "-" + "0"+room; //format roomno as string
-					for(int k=0;k<R.size();k++)
-					{
-						//1st check whether there's any reservation for this room
-						Reservation temp = R.get(k);
-						
-						if(temp.bedType!=null) //to ensure that the roomtype is not null
-						{
-							
-							if(temp.bedType.equals(roomNo))//if it's the same room
-							{
-								if(!(D.compareTo(temp.check_in)>=0 && D.compareTo(temp.check_out)<0))
-								{
-									return roomNo;
-								
-								}
-							}
-				
-						}		
-					}
-					room++;
-				}	
-				floor++;
-			}
-			break;
-			
+			return SearchRoomFunction(D,R,4,1,3,8);
 		}
 		
 
@@ -375,4 +86,81 @@ public class Validation {
 		return null;
 	}
 	
+	public String SearchRoomFunction(Date D,ArrayList<Reservation> R,int startingFloor,int startingRoom, int NoOfFloor, int NoOfRoomPerFloor)
+	{
+		//1st check whether there's any reservation for this room
+		int floor = startingFloor;
+		int room = startingRoom;
+		
+		boolean roomBook=false; //this is to check whether there's this room in the reservation
+		
+		for(int t=0;t<NoOfFloor;t++)
+		{
+			for(int i=0;i<NoOfRoomPerFloor;i++) //this forloop will run through all room
+			{
+				String roomNo = "0"+floor + "-" + "0"+room;
+				for(int k=0;k<R.size();k++)
+				{
+					
+					//1st check whether there's any reservation for this room
+					Reservation temp = R.get(k);
+					
+					if(temp.bedType!=null)
+					{
+						if(temp.bedType.equals(roomNo))
+						{
+							roomBook=true; //this room exist
+						}
+					}
+					
+				}
+				
+				if(roomBook==true)
+				{
+				room++;
+				roomBook=false; //reset counter
+				}
+				else
+					return roomNo; //there's no this room in reservation so can book
+			}
+			floor++;
+		}
+		
+		
+		
+		//2nd if the room exist in reservation, then check their date
+		floor = startingFloor;
+		room = startingRoom;
+		
+		for(int t=0;t<NoOfFloor;t++)
+		{
+			for(int i=0;i<NoOfRoomPerFloor;i++) //this forloop will run through all room
+			{
+				String roomNo = "0"+floor + "-" + "0"+room; //format roomno as string
+				for(int k=0;k<R.size();k++)
+				{
+					//1st check whether there's any reservation for this room
+					Reservation temp = R.get(k);
+					
+					if(temp.bedType!=null) //to ensure that the roomtype is not null
+					{
+						
+						if(temp.bedType.equals(roomNo))//if it's the same room
+						{
+							if(!(D.compareTo(temp.check_in)>=0 && D.compareTo(temp.check_out)<0))
+							{
+								return roomNo;
+							
+							}
+						}
+			
+					}		
+				}
+				room++;
+			}	
+			floor++;
+		}
+		
+		return null;
+	}
 }
