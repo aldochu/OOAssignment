edba7c11770ed240842b0dc1eaa05d26488e061b
aldochu <aldochu@gmail.com>
2018-04-10 17:42:28 +0800

    added function in validation to get the occupancy rate on any previous date

--- src/HRPS/Validation.java
+++ src/HRPS/Validation.java
@@ -1,5 +1,6 @@
 package HRPS;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -25,6 +26,21 @@ public class Validation {
 		
 		Date currentDatePlusk = c.getTime();
 		return(D.compareTo(currentDatePlusk)>0);
+
+	}
+	
+	public int CheckDateWithin(Date D,ArrayList<Payment> p) //this will check D with the current date + k
+	{
+		int number = 0;
+		
+		for(int i=0;i<p.size();i++)
+		{
+		Payment temp = p.get(i);
+		if(D.compareTo(temp.checkInDate)>=0 && D.compareTo(temp.checkOutDate)<=0)
+			number++;
+		}
+		
+		return number;
 
 	}
 	
